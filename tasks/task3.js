8991a4e98a2ca61d63089c5f72a18c768ff6329b
Artem <Art.Tikhonenko@gmail.com>
2024-02-12 10:06:13 +0300

    task3

---
+++ tasks/task3.js
@@ -0,0 +1,78 @@
+// Место для первой задачи
+function firstTask() {
+    for( let i = 5; i <= 10; i++) {
+        console.log(i);
+    }
+    
+    
+}
+
+// Место для второй задачи
+function secondTask() {
+    for(let i = 20; i > 10; i--) {
+
+        if(i === 13) {
+            break;
+        } else {
+            console.log(i);
+        }
+    }    
+    
+    
+}
+
+// Место для третьей задачи
+function thirdTask() {
+    for( let i = 2; i <= 10; i++) {
+        if(i % 2 === 1) {
+            continue;
+        } else {
+            console.log(i);
+        }
+    }   
+}
+
+// Место для четвертой задачи
+
+// Цикл, который нужно переписать:
+
+for (let i = 2; i <= 16; i++) {
+    if (i % 2 === 0) {
+        continue;
+    } else {
+        console.log(i);
+    }
+}
+
+function fourthTask() {
+
+    let i = 2;
+    while(i <= 16) {
+        if (i % 2 === 0) {
+            i++;
+            continue;
+        } else {
+            console.log(i);
+        }
+        i++;
+    }
+    
+}
+
+// Место для пятой задачи
+
+function fifthTask() {
+    const arrayOfNumbers = [];
+
+    // Пишем решение вот тут
+    for( let i = 5; i <= 10; i++) {
+        arrayOfNumbers[i - 5] = i;
+    }
+
+    console.log(arrayOfNumbers);
+    
+    // Не трогаем
+    return arrayOfNumbers;
+}
+
+fifthTask();
