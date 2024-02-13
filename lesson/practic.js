7016a67db0ea17c4210146c2ded49b23b7d06efa
Artem <Art.Tikhonenko@gmail.com>
2024-02-13 10:50:24 +0300

    task6

--- lesson/practic.js
+++ lesson/practic.js
@@ -1,15 +1,2 @@
 'use strict';
 
-let res = '';
-const length = 7;
-
-for ( let i = 1; i < length; i++) {
-    
-    for( let j = 0; j < i; j++) {
-        res += '*';
-    }
-
-    res = res + '\n';
-}
-
-console.log(res);
