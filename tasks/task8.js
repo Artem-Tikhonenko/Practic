9d05c257ddcd280c537e9c7896dc425cb16fccef
Artem <Art.Tikhonenko@gmail.com>
2024-02-14 10:05:14 +0300

    getTimeFromMinutes

---
+++ tasks/task8.js
@@ -0,0 +1,36 @@
+// Место для первой задачи
+function getTimeFromMinutes(minTotal) {
+    if(typeof(minTotal) !== 'number' || minTotal < 0 || !Number.isInteger(minTotal)) {
+        return "Ошибка, проверьте данные"
+    }
+
+    let hours = Math.floor(minTotal / 60);
+    let minutes = minTotal % 60;
+
+    let hoursStr = '';
+
+    switch(hours) {
+        case 0:
+            hoursStr = 'часов';
+            break;
+        case 1:
+            hoursStr = 'час';
+            break;
+        case 2:
+        case 3:
+        case 4:
+            hoursStr = 'часа';
+            break;
+        default:
+            hoursStr = 'часов';
+    }
+
+    return `Это ${hours} ${hoursStr} и ${minutes} минут`;
+}
+
+console.log(getTimeFromMinutes(150));
+
+// Место для второй задачи
+function findMaxNumber() {
+
+}
