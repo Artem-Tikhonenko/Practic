90b5247cb35371a53f171d98130b7598d156d6fc
Artem <Art.Tikhonenko@gmail.com>
2024-02-19 10:24:52 +0300

    refactoring path 4

--- lesson/practic.js
+++ lesson/practic.js
@@ -1,2 +1,14 @@
 'use strict';
 
+const soldier = {
+    health: 500,
+    armor: 222,
+    hi: function(){
+        console.log('hiiiiii');
+    }
+}
+
+const john = Object.create(soldier);
+
+// console.log(john.armor);
+john.hi();
