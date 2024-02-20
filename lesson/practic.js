f3a70cd999d980487c7de8b7fd49b64403a1ecc7
Artem <Art.Tikhonenko@gmail.com>
2024-02-20 14:28:31 +0300

    interview tasks

--- lesson/practic.js
+++ lesson/practic.js
@@ -1,14 +1,2 @@
 'use strict';
 
-const soldier = {
-    health: 500,
-    armor: 222,
-    hi: function(){
-        console.log('hiiiiii');
-    }
-}
-
-const john = Object.create(soldier);
-
-// console.log(john.armor);
-john.hi();
---
+++ tasks/interview tasks.js
@@ -0,0 +1,25 @@
+let x = 5; alert( x++ ); //5
+
+console.log([ ] + false - null + true)  // NaN
+
+let y = 1; 
+let x = y = 2; 
+console.log(x); //2
+
+console.log([] + 1 + 2) //12
+
+console.log('1'[0])  // 1
+
+console.log(2 && 1 && null && 0 && undefined )  // null
+
+console.log(!!( 1 && 2 ) === (1 && 2)) // false
+
+console.log(( null || 2 && 3 || 4 )) // 3
+
+a = [1, 2, 3]; 
+b = [1, 2, 3]; 
+console.log(a == b); //false
+
+console.log( +"Infinity" );  //Infinity
+
+console.log(0 || "" || 2 || undefined || true || falsе )  // 2
