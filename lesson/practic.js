f42321959c112f45b85e290c4a7e54452996c057
Artem <Art.Tikhonenko@gmail.com>
2024-03-18 11:39:24 +0300

    recusion

--- lesson/practic.js
+++ lesson/practic.js
@@ -1,2 +1,54 @@
 'use strict';
 
+
+let students = {
+    js: [{
+        name: 'Jonh',
+        progress: 100
+    },{
+        name: 'Ivan',
+        progress: 60
+    }],
+
+    html: {
+        basic: [{
+            name: 'Peter',
+            progress: 20
+        }, {
+            name: 'Ann',
+            progress: 15
+        }],
+
+        pro: [{
+            name: 'Sam',
+            progress: 10
+        }]
+    }
+}
+
+function getTotal(data) {
+    let total = 0;
+    let students = 0;
+
+    for (let course of Object.values(data)) {
+        if (Array.isArray(course)) {
+            students += course.length;
+
+            for (let i = 0; i < course.length; i++) {
+                total += course[i].progress;
+            }
+        } else {
+            for (let subCourse of Object.values(course)) {
+                students += subCourse.length;
+
+                for (let i = 0; i < subCourse.length; i++) {
+                    total += subCourse[i].progress;
+                }
+            }
+        }
+    }
+
+    return total / students;
+}
+
+console.log(getTotal(students));
