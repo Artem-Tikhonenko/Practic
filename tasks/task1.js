312619b200515412651d6fdac00d67ce69a21c8a
Artem <Art.Tikhonenko@gmail.com>
2024-03-19 10:03:20 +0300

    task 16 recursion

---
+++ tasks/task1.js
@@ -0,0 +1,13 @@
+function factorial(n) {
+    if(typeof(n) !== 'number' || !Number.isInteger(n)) {
+        return 'Error';
+    }
+
+    if(n >= 1) {
+        return n * factorial(n - 1);
+    } else {
+        return 1;
+    }
+}
+
+console.log(factorial(6));
