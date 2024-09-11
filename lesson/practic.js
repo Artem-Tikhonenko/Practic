b648551f880316ef85e58ba5d4fdf17451155629
Artem Ti <Art.Tikhonenko@gmail.com>
2024-09-11 11:48:05 +0300

    /

--- lesson/practic.js
+++ lesson/practic.js
@@ -1,1 +1,20 @@
 'use strict';
+let counter = 0;
+
+function createConter() {
+    counter = 1;
+
+    const myFunction = function() { 
+        counter = counter + 1; 
+        return counter; 
+    }
+
+    return myFunction;
+}
+
+const incr = createConter(); 
+const c1 = incr();
+const c2 = incr();
+const c3 = incr();
+
+console.log(c1,c2,c3);
