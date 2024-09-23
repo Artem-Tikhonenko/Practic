1753a9d7c1422cf698fe1181632c394bb15ba6e4
Artem Ti <Art.Tikhonenko@gmail.com>
2024-09-23 13:21:52 +0300

    .

--- elements/js/script.js
+++ elements/js/script.js
@@ -3,15 +3,19 @@
 const box = document.getElementById('box'),
       btns = document.getElementsByTagName('button'),
       circle = document.getElementsByClassName('circle'),
+      wrapper = document.querySelector('.wrapper'),
       hearts = document.querySelectorAll('.heart'),
       oneHeart = document.querySelector('.heart');
 
 console.log(box);
 console.log(btns);
 console.log(circle);
+console.log(wrapper);
 console.log(hearts);
 console.log(oneHeart);
 
 hearts.forEach(item => {
-    console.log(item);
+    item.style.color = 'green';
 });
+
+oneHeart.style.backgroundColor = 'green';
