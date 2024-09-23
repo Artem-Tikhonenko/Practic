bff7d1b8d4b026cbe0a3be3f22563004e7b12d71
Artem Ti <Art.Tikhonenko@gmail.com>
2024-09-23 10:22:54 +0300

    elemetns

--- elements/js/script.js
+++ elements/js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const box = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'),
+      circle = document.getElementsByClassName('circle'),
+      hearts = document.querySelectorAll('.heart'),
+      oneHeart = document.querySelector('.heart');
+
+console.log(box);
+console.log(btns);
+console.log(circle);
+console.log(hearts);
+console.log(oneHeart);
+
+hearts.forEach(item => {
+    console.log(item);
+});
