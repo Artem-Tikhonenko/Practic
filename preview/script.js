d82c7091548560c4e85e321d8f81f5a9d7db3e08
Artem Ti <Art.Tikhonenko@gmail.com>
2024-10-01 11:20:17 +0300

    recursion

--- preview/script.js
+++ preview/script.js
@@ -1,7 +1,25 @@
-// console.log(document.body.childNodes);
-for (let node of document.body.childNodes) {
-    if (node.nodeName == "#text" || node.nodeName == "#comment") {
-        continue;
+// function pow(x, n) {
+//     if (n === 1) {
+//         return x;
+//     } else {
+//         return x * pow(x, n - 1);
+//     }
+// }
+
+// console.log(pow(2, 3));
+// console.log(pow(2, 4));
+// console.log(pow(2, 5)); // 32
+// console.log(pow(2, 6)); // 64
+
+
+function factorial(n) {
+
+    if(n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
     }
-    console.log(node);
 }
+
+const fact = factorial(6);
+console.log(fact);
