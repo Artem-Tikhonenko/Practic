536d1028c13b5f2473a238669cbadb32abb7c225
Artem Ti <Art.Tikhonenko@gmail.com>
2024-10-03 10:38:49 +0300

    classList

--- preview/script.js
+++ preview/script.js
@@ -1,12 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
+const btns = document.querySelectorAll('button'),
+      wrapper = document.querySelector('.btn-block'),
+      body = document.querySelector('body');
 
-    const box = document.querySelector('.box');
-
-    box.addEventListener('touchstart', (e) => {
-        e.preventDefault();
-
-        console.log('start');
-        e.target.style.background = 'blue';
-    });
-
+btns[0].addEventListener('click', (e) => {
+    if (!btns[1].classList.contains('red')) {
+        btns[1].classList.add('red');
+    } else {
+        btns[1].classList.remove('red');
+    }
 });
+
+wrapper.addEventListener('click', (event) => {
+    if (event.target && event.target.classList == 'red') {
+        console.log('hello');
+    }
+});
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
