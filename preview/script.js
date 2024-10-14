f8a8c2470daac3c57d6eef319fb6472b7d427868
Artem Ti <Art.Tikhonenko@gmail.com>
2024-10-14 11:29:15 +0300

    JSON

--- preview/script.js
+++ preview/script.js
@@ -1,31 +1,15 @@
 'use strict';
 
-class Rec {
-    constructor(width, height) { //свойства
-        this.width = width;
-        this.height = height;
-    }
-
-    calcArea() { //метод
-        return this.width * this.height;
+const obj = {
+    name: 'Alex',
+    tel: '+223232323',
+    parents: {
+        mom: 'Olga',
+        dad: 'Mike'
     }
 }
 
-class RecTextColor extends Rec {
-    constructor(width, height, text, color) {
-        super(width, height);  //вызывает то что было у родителя 
-        this.text = text;
-        this.color = color;
-    }
-
-    showMyProps() {
-        return `Текст: ${this.text}, цвет: ${this.color}`;
-    }
-}
-
-const r1 = new Rec(10, 10);
-const r2 = new RecTextColor(5, 5, 'HELLO', 'blue');
-
-
-console.log(r1, r1.calcArea());  // Rec { width: 10, height: 10 } 100
-console.log(r2, r2.showMyProps(), r2.calcArea()); // RecTextColor { width: 5, height: 5, text: 'HELLO', color: 'blue' } Текст: HELLO, цвет: blue 25
+const clone = JSON.parse(JSON.stringify(obj));
+clone.parents.mom = 'Ann';
+console.log(obj);
+console.log(clone);
